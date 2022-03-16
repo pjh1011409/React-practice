e3c3159fc8191e7fb5cb1f6fc3c51ff64c26fd68
ParkJeongHo <pjh31208@naver.com>
2022-03-16 12:52:32 +0900

    First_Done

--- blog/src/App.js
+++ blog/src/App.js
@@ -10,11 +10,10 @@ function App() {
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(false); //모달창을 켜고 닫는 스위치
   let [누른제목, 누른제목변경] = useState(0);
-  // function 제목바꾸기(){
-  //   let newArray = [...글제목]; //글제목에 있던 0번째 데이터를 여자코트추천으로 바꿈
-  //   newArray[0] = '여자코트 추천'
-  //   글제목변경(newArray);
-  // }
+  let [입력값, 입력값변경] = useState('');
+
+
+   
 
     return (
      
@@ -22,31 +21,12 @@ function App() {
         <div className="black-nav">
             <div>개발 Blog</div>
         </div>
-        {/* <div className="list">
-          <h3> { 글제목[0] } 
-           <button onClick = {제목바꾸기} >버튼</button>
-            <span onClick= {()=> { 따봉변경(따봉+1)}}>👍</span> {따봉} 
-          </h3>
-
-          <p>2월 17일 발행</p>
-          <hr/>
-        </div>
-        <div className="list">
-          <h3> { 글제목[1] }</h3>
-          <p>2월 17일 발행</p>
-          <hr/>
-        </div>
-        <div className="list">
-          <h3> { 글제목[2] }</h3>
-          <p>2월 17일 발행</p>
-          <hr/>
-        </div> */}
-
+     
         { //for문과 같은 역할로 map을 이용해 글제목 배열의 데이터가 글이라는 파라미터에 담기고
           // 배열에 다른 제목을 가진 글제목이 차례로 출력된다.
           글제목.map((글, i)=>{
               return (
-              <div className="list">
+              <div className="list" key={i}>
                 <h3 onClick = {() => { 누른제목변경(i)}}> 
                   {글}   
                 <span onClick= {()=> { 따봉변경(따봉+1)}}>👍</span> {따봉} 
@@ -57,13 +37,20 @@ function App() {
               )
           })
         }
+
+          <div className='publish'>
+            <input onChange={(e) => {입력값변경(e.target.value)}}/>
+            <button onClick = {()=>{
+              let 제목추가 = [...글제목];
+              제목추가.unshift(입력값);
+              글제목변경(제목추가);
+            }}>저장</button>
+          </div>
+
+        
+
           {/* 버튼클릭시 모달 열고 닫힘     */}
           <button onClick = {()=>{modal변경(!modal)}}>버튼</button>
-
-
-
-
-
 
 
         { //if문과 같은 역할로 {} 안에 작성하고 true이면 모달이 보이게, false면 빈 페이지
